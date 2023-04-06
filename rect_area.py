eecfe8d7d49e809bef1da44b6bc0e2ed11239b55
raegansweeney <129887785+raegansweeney@users.noreply.github.com>
2023-04-06 11:40:07 -0700

    Add files via upload

---
+++ rect_area.py
@@ -0,0 +1,17 @@
+# Raegan Sweeney
+# GitHub Assignment
+
+# rectangle area function
+# takes 2 inputs and returns rectangle angle
+
+def rect_area():
+    # take length and width
+    length = 12
+    width = 10
+
+    # compute area of rectangle
+    area = length*width
+    print(f'the area of the rectangle is {area}')
+
+    # return
+    return area
