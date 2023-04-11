4d32c43c6d4c2f89b99b537e61f3337cb64ee590
raegansweeney <raegan3339@gmail.com>
2023-04-11 11:31:09 -0700

    For CS 162 Week 2 Assignment for graphic file practice

---
+++ cs161assn1.py
@@ -0,0 +1,44 @@
+# Raegan Sweeney
+# CS 161
+# Assignment 1
+
+##########################################################################
+# Question 1
+print(1)
+print("Raegan Sweeney")
+
+##########################################################################
+# Question 2
+print(2)
+print("Bend, Oregon")
+
+##########################################################################
+# Question 3
+print(3)
+print(31*24*60*60)
+
+##########################################################################
+# Question 4
+print(4)
+
+# egg var
+eggs = 403
+
+# do math and print
+dozens = eggs // 12
+remainder = eggs % 12
+print(f"starting eggs: {eggs}")
+print(f"there would be {dozens} dozens of eggs with a remainder of {remainder} eggs")
+
+##########################################################################
+# Question 5
+print(5)
+
+# variables
+name = "Raegan Sweeney"
+cs_class = "CS 161"
+
+# print output
+print (f"My name is {name} and the class I'm in is {cs_class}!")
+
+##########################################################################
